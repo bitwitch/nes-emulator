5e243750ea16c22df878982c057875ac7db73559
shaw <shawwian@gmail.com>
2022-04-07 17:05:06 -0400

    removed unused error utils

--- error.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <errno.h>
-
-void panic(char *msg) {
-    fprintf(stderr, "Error: %s: %s\n", msg, strerror(errno));
-    exit(1);
-}
--- error.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ERROR_H
-#define __ERROR_H
-
-void panic(char *msg);
-
-#endif
