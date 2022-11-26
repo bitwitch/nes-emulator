5422a836a4f909b2539d6fb3f02526f5014244e6
shaw <shawwian@gmail.com>
2022-11-26 00:01:57 -0500

    both pulse channels roughly working

--- apu.h
+++ apu.h
@@ -2,11 +2,14 @@
 #define __APU_H__
 
 #include <stdint.h>
+#include <stdbool.h>
 
 void apu_init(void);
 uint8_t apu_read(uint16_t addr);
 void apu_write(uint16_t addr, uint8_t data);
 void apu_tick(void);
 void apu_render_sound_wave(void);
+bool apu_request_frame(void);
+void apu_flush_sound_buffer(void);
 
 #endif
