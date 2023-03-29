d32f275923f547097d26b766bc0aa7680d9ffaf1
Shaw <shawwian@gmail.com>
2023-03-29 17:15:20 -0400

    windows build using cl

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 pushd .\build-windows
 xcopy /D ..\font.png .
-setlocal EnableDelayedExpansion
-setlocal EnableExtensions
+setlocal EnableExtensions EnableDelayedExpansion
 set sources=
 for %%i in (..\src\*.c) do set sources=!sources! %%i
 
 cl %sources% /DSDL_MAIN_HANDLED SDL2main.lib SDL2.lib /Zi /W2 /nologo /Fenes /I.. /I..\src /ISDL2\include /link /LIBPATH:SDL2\lib
 
+endlocal
 popd
